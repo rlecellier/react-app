2c660bc7f6aed6ac708032ca6485338241c39baf
Romain Le Cellier <romain.le-cellier@passculture.app>
2023-01-16 16:12:57 +0100

    chore: configure eslint and prettier for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: [
+    'src/api/**/gen/*',
+    '**/*.svg',
+    '**/*.css',
+    '**/*.scss',
+    '**/*.md',
+    '**/*.jpg',
+    '**/*.png',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 6,
+  },
+  rules: {
+    curly: ['error', 'all'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-console': 1,
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['.'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+}
