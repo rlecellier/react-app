33f3fbfebc1594ed6cf227029c9da3d2c0b1b2d4
Romain Le Cellier <romain.le-cellier@passculture.app>
2023-01-18 14:37:54 +0100

    fix: tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Home from './Home'
 
+const renderHome = () => {
+  return render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
+}
+
 test('renders title', () => {
-  render(<Home />)
+  renderHome()
   const titleElement = screen.getByText(/Home Page/i)
   expect(titleElement).toBeInTheDocument()
 })
--- src/pages/SignIn/SignIn.test.tsx
+++ src/pages/SignIn/SignIn.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import SignIn from './SignIn'
 
+const renderSignIn = () => {
+  return render(
+    <BrowserRouter>
+      <SignIn />
+    </BrowserRouter>
+  )
+}
+
 test('renders title', () => {
-  render(<SignIn />)
+  renderSignIn()
   const titleElement = screen.getByText(/SignIn Page/i)
   expect(titleElement).toBeInTheDocument()
 })
